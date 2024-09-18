3b803df1965d12d7709a7bbd283b296de54d7495
Kunaljolly <134881423+Kunaljolly@users.noreply.github.com>
2024-09-18 17:49:28 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        snums = list(map(str, nums))
+        print(snums)
+        snums.sort(key=lambda x: x*10, reverse = True)
+
+        if snums[0] == "0":
+            return "0"
+        
+        return ''.join(snums)
